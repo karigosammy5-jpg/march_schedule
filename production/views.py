0481e38c411a3f5c14b9072266152dad6bb2c720
karigosammy5 <karigosammy5@gmail.com>
2026-03-21 22:40:34 +0300

    UI fix : resolved row looping, restored models and added solid black headers

--- production/views.py
+++ production/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from .models import MarchProductionTask
-from django.db.models import Sum
 
 def dashboard(request):
-    tasks = MarchProductionTask.objects.all()
-    grand_total = tasks.aggregate(Sum('daily_quantity'))['daily_quantity__sum'] or 0
+    all_tasks = MarchProductionTask.objects.all().order_by('production_date')
+    
+    # We create a dictionary to group everything by Lot Number
+    # Key: lot_number, Value: dictionary of all the lot's info
+    n_series_grouped = {}
+    f_series_grouped = {}
+
+    for task in all_tasks:
+        target_group = n_series_grouped if task.line == 'N' else f_series_grouped
+        
+        if task.lot_number not in target_group:
+            target_group[task.lot_number] = {
+                'model_name': task.model_name,
+                'lot_number': task.lot_number,
+                'ckd_arrival_iea': task.ckd_arrival_iea,
+                'prd_customer': task.prd_customer,
+                'lot_total_units': task.lot_total_units,
+                'days': {} # We'll store the daily quantities here
+            }
+        
+        # Assign the quantity to the specific day
+        target_group[task.lot_number]['days'][task.production_date.day] = task.daily_quantity
 
     context = {
-        'tasks': tasks,
-        'grand_total': grand_total,
+        'n_series_lots': n_series_grouped.values(),
+        'f_series_lots': f_series_grouped.values(),
+        'march_days': range(1, 32),
     }
-
     return render(request, 'production/dashboard.html', context)
